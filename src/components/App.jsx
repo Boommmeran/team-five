9454dbc79c1a1b734261ed51e6776e5f43ad5143
Mykhailo Volontyr <boommmeran@gmail.com>
2024-01-31 23:41:17 +0200

    project preparation for deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      Team-five
     </div>
   );
 };
