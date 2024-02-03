ba9c9b2a475ebb72f7315fb917530ada8d9c37a8
Natali Nesterenko <natalikagami@gmail.com>
2024-02-03 23:29:46 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -22,10 +22,10 @@ const theme = {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={ theme }>
+    <ThemeProvider theme={theme}>
       <BrowserRouter basename="/team-five">
         <App />
-        <GlobalStyles />
+        <GlobalStyles />{' '}
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
