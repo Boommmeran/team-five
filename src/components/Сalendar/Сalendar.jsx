d21fbeab1bde4f045e0ede892f175d5c94ceee08
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2024-02-04 17:56:11 +0200

    add components associated with cards

---
+++ src/components/Сalendar/Сalendar.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function Calendar(){
+
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      dateFormat="MMMM d"
+    />
+  );
+}
