ca45379feac7795e712b4f76f8a3f678baca030d
Mykhailo Volontyr <boommmeran@gmail.com>
2024-02-06 23:45:45 +0200

    bug fix

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,1 +1,1 @@
-export { default } from './LoginForm.jsx';
+export { default } from './LoginForm';
--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -1,1 +1,1 @@
-export { default } from './RegisterForm.jsx';
+export { default } from './RegisterForm';
