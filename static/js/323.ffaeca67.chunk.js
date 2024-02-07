3a1b54d04c0759ca6a845bd0a57c060614ea9ae1
Boommmeran <108887968+Boommmeran@users.noreply.github.com>
2024-02-07 15:34:44 +0000

    Deploying to gh-pages from @ Boommmeran/team-five@f432a0fafad1fc1b89f6b170b259a43c38cac2d5 🚀

---
+++ static/js/323.ffaeca67.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkteam_five=self.webpackChunkteam_five||[]).push([[323],{8323:function(e,i,t){t.d(i,{J:function(){return a}});t(2791);var n=t.p+"static/media/sprite.dc7add122f0c0a373750a30cb14fce1a.svg",c=t(184),a=function(e){var i=e.fill,t=void 0===i?"none":i,a=e.stroke,s=void 0===a?"inherit":a,o=e.name,r=e.width,d=void 0===r?"18px":r,f=e.height,h=void 0===f?"18px":f;return(0,c.jsx)("svg",{fill:t,stroke:s,width:d,height:h,children:(0,c.jsx)("use",{href:"".concat(n,"#icon-").concat(o)})})}}}]);
+//# sourceMappingURL=323.ffaeca67.chunk.js.map
