0d0027f51f70b440e46bcb4800e4d4cd8df2b3a9
Mykhailo Volontyr <boommmeran@gmail.com>
2024-02-07 07:07:51 +0200

    restore deleted folders

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,112 @@
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { registerUser } from '../../redux/auth/authOperation';
+
+import { useState } from 'react';
+import { Icon } from 'components/Icon';
+import {
+  // IconStyledEye,
+  LabelStyled,
+  RegInputStyled,
+  RegisterStyled,
+  WrapForRegNav,
+} from './RegisterForm.styled';
+
+export const RegisterForm = () => {
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    // reset,
+    // watch,
+    formState: { errors, isValid },
+  } = useForm({ mode: 'onBlur' });
+  const onSubmit = data => {
+    dispatch(
+      registerUser({
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      })
+    );
+  };
+
+  return (
+    <RegisterStyled>
+      <WrapForRegNav>
+        <NavLink to="/auth/register">Registration</NavLink>
+        <NavLink to="/auth/login">Login</NavLink>
+      </WrapForRegNav>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Name
+          <RegInputStyled
+            {...register('name', {
+              required: 'Required field',
+              minLength: {
+                value: 2,
+                message: 'Name is too short',
+              },
+            })}
+          ></RegInputStyled>
+        </label>
+        <div>{errors?.name && <p>{errors?.name?.message || 'Error'}</p>}</div>
+        <label>
+          Email
+          <RegInputStyled
+            name="email"
+            type="email"
+            {...register('email', {
+              required: 'Required field',
+              pattern: /^\S+@\S+$/i,
+            })}
+          ></RegInputStyled>
+        </label>
+        <div>{errors?.email && <p>{errors?.email?.message || 'Error'}</p>}</div>
+        <LabelStyled style={{ stroke: 'red' }}>
+          Password
+          <RegInputStyled
+            name="password"
+            type={passwordShown ? 'text' : 'password'}
+            {...register('password', {
+              required: 'Required field',
+              minLength: {
+                value: 8,
+                message: 'Password is too short',
+              },
+            })}
+          ></RegInputStyled>
+          <i
+            onClick={() => {
+              setPasswordShown(!passwordShown);
+            }}
+          >
+            {' '}
+            <Icon name="eye" />
+          </i>
+        </LabelStyled>
+        <div>
+          {errors?.password && <p>{errors?.password?.message || 'Error'}</p>}
+        </div>
+        {/* <RegInputStyled
+          name="confirmPassword"
+          type="password"
+          {...register('confirm_password', {
+            required: true,
+            validate: val => {
+              if (watch('password') !== val) {
+                return 'Your passwords do no match';
+              }
+            },
+          })}
+        /> */}
+
+        <input type="submit" disabled={!isValid} />
+      </form>
+    </RegisterStyled>
+  );
+}
---
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -0,0 +1,59 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const LabelStyled = styled.label`
+  position: relative;
+`;
+export const RegisterStyled = styled.div`
+  border-radius: 8px;
+  min-height: 363px;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+
+  padding: 34px 29px 34px 29px;
+  border: 1px solid black;
+  margin-left: auto;
+  margin-right: auto;
+  @media only screen and (min-width: 375px) {
+    width: 335px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 424px;
+    height: 332px;
+    padding: 49px 42px 49px 42px;
+  }
+`;
+export const RegInputStyled = styled.input`
+  display: flex;
+  width: 287px;
+  height: 49px;
+  color: red;
+  border: 1px solid black;
+  border-radius: 8px;
+  @media only screen and (min-width: 768px) {
+    width: 344px;
+  }
+`;
+export const IconStyledEye = styled.i`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+export const StyledNavLinkRegister = styled(NavLink)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Poppins-medium', sans-serif;
+  color: rgba(255, 255, 255, 0.3);
+  font-size: 18px;
+`;
+export const WrapForRegNav = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: auto;
+  gap: 15px;
+  margin-bottom: 42px;
+`;
