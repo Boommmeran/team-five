5f7608d4c4ab962bb33b48bfaf9a42db463b0a34
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2024-02-09 12:34:55 +0200

    add style calendar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,6 @@
 import { Header } from 'components/Header';
 import { Sidebar } from 'components/SideBar';
 import { ScreensPage } from 'components/ScreensPage';
-import Card from 'components/Card/Card';
-import AddCardModal from 'components/AddCardModal/AddCardModal';
 
 export default function HomePage() {
   return (
