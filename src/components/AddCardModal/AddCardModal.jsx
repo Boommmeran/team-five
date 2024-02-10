6e10cb97004d9d31b4ffd2daff22c62d664e387b
Mykhailo Volontyr <boommmeran@gmail.com>
2024-02-10 20:07:35 +0200

    fix add-card component

--- src/components/AddCardModal/AddCardModal.jsx
+++ src/components/AddCardModal/AddCardModal.jsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { Calendar } from 'components/Сalendar';
+import { Calendar } from 'components/Calendar';
 import { Icon } from 'components/Icon';
 
 import {
@@ -37,84 +38,95 @@ const formCardSchema = Yup.object().shape({
     .max(500, 'Too Long!')
     .required(`This field is required.`),
   colorPriority: Yup.string().oneOf(priority),
-  dealline: Yup.string(),
+  deadline: Yup.date(),
 });
 
 export default function AddCardModal({ title, btnText, onClose, reqFunc }) {
-  // тут має бути логіка отримання значень вже існуючої
-  // картки і подальший запис в initValues наприклад через оператор ??
-  // тобто треба переписати initialValues у формат
-  // "збережені дані" (для випадку Edit Card) ?? "початкові значення" (для випадку Add Card)
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const initValues = {
-    title: '',
-    description: '',
-    colorPriority: 'without',
-    dealline: '',
-  };
   const onSubmit = values => {
     reqFunc(values);
     onClose();
-    console.log(values);
   };
+
   return (
     <Container>
       <ModalBody>
         <TitleModal>{title}</TitleModal>
         <CloseModal>
           <button type="button" onClick={onClose}>
-            <div style={{ stroke: 'var(--primaryTextColor)' }}>
-              <Icon name="close" width="18" height="18" />
-            </div>
+              <Icon
+                name="close"
+                width="18"
+                height="18"
+                stroke="var(--primaryTextColor)"
+              />
           </button>
         </CloseModal>
         <Formik
-          initialValues={initValues}
+          initialValues={{
+            title: '',
+            description: '',
+            colorPriority: 'without',
+            deadline: selectedDate,
+          }}
           validationSchema={formCardSchema}
           onSubmit={onSubmit}
         >
-          <StyledForm>
-            <TitleCard type="text" name="title" placeholder="Title" />
-            <ErrMsg component="p" name="title" />
-            <Label>
-              <StyledDescription
-                rows={4}
-                component="textarea"
-                name="description"
-                placeholder="Description"
-              />
-              <ErrMsg component="p" name="description" />
-            </Label>
-            <LabelColorStyle id="colorCard-radio-group">
-              Label color
-            </LabelColorStyle>
-            <StyleRadioButton role="group" aria-labelledby="my-radio-group">
-              <RadioButtonBlu>
-                <RadioButton type="radio" name="colorPriority" value="low" />
-              </RadioButtonBlu>
-              <RadioButtonRed>
-                <RadioButton type="radio" name="colorPriority" value="medium" />
-              </RadioButtonRed>
-              <RadioButtonGreen>
-                <RadioButton type="radio" name="colorPriority" value="high" />
-              </RadioButtonGreen>
-              <RadioButtonGrey>
-                <RadioButton
-                  type="radio"
-                  name="colorPriority"
-                  value="without"
+          {({ handleSubmit, setFieldValue }) => (
+            <StyledForm onSubmit={handleSubmit}>
+              <TitleCard type="text" name="title" placeholder="Title" />
+              <ErrMsg component="p" name="title" />
+              <Label>
+                <StyledDescription
+                  rows={4}
+                  component="textarea"
+                  name="description"
+                  placeholder="Description"
                 />
-              </RadioButtonGrey>
-            </StyleRadioButton>
-            <DeadlineStyle>Deadline</DeadlineStyle>
-            <Calendar />
-            <AddButton type="submit">
-              <StylePlus>
-                <Icon name="plus" width="14" height="14" />
-              </StylePlus>
-              <p> {btnText}</p>
-            </AddButton>
-          </StyledForm>
+                <ErrMsg component="p" name="description" />
+              </Label>
+              <LabelColorStyle id="colorCard-radio-group">
+                Label color
+              </LabelColorStyle>
+              <StyleRadioButton role="group" aria-labelledby="my-radio-group">
+                <RadioButtonBlu>
+                  <RadioButton type="radio" name="colorPriority" value="low" />
+                </RadioButtonBlu>
+                <RadioButtonRed>
+                  <RadioButton
+                    type="radio"
+                    name="colorPriority"
+                    value="medium"
+                  />
+                </RadioButtonRed>
+                <RadioButtonGreen>
+                  <RadioButton type="radio" name="colorPriority" value="high" />
+                </RadioButtonGreen>
+                <RadioButtonGrey>
+                  <RadioButton
+                    type="radio"
+                    name="colorPriority"
+                    value="without"
+                  />
+                </RadioButtonGrey>
+              </StyleRadioButton>
+              <DeadlineStyle>Deadline</DeadlineStyle>
+              <Calendar
+                selectedDate={selectedDate}
+                onDateChange={date => {
+                  setSelectedDate(date);
+                  setFieldValue('deadline', date);
+                }}
+              />
+              <AddButton type="submit">
+                <StylePlus>
+                  <Icon name="plus" width="14" height="14" />
+                </StylePlus>
+                <p>{btnText}</p>
+              </AddButton>
+            </StyledForm>
+          )}
         </Formik>
       </ModalBody>
     </Container>
