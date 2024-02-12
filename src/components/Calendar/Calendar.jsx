4092d6cadad19c2c97ef8f92bb6b3bc51062c8b2
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-02-12 12:52:04 +0100

    added css for calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,5 +1,8 @@
 import DatePicker from 'react-datepicker';
+import { enGB } from 'date-fns/locale';
 import 'react-datepicker/dist/react-datepicker.css';
+
+import 'components/Calendar/Calendar.css';
 
 export function Calendar({ selectedDate, onDateChange }) {
   const handleDateChange = date => {
@@ -12,6 +15,8 @@ export function Calendar({ selectedDate, onDateChange }) {
       onChange={handleDateChange}
       dateFormat="MMMM d"
       minDate={new Date()}
+      locale={enGB}
+      weekStartsOn={1}
     />
   );
 }
