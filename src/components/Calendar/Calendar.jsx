d308f2f813c9937c1d16aba8d98360e143f0fdd3
Mykhailo Volontyr <108887968+Boommmeran@users.noreply.github.com>
2024-02-12 22:32:13 +0200

    Revert "fix style card"

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,6 +1,5 @@
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import 'components/Calendar/Calendar.css';
 
 export function Calendar({ selectedDate, onDateChange }) {
   const handleDateChange = date => {
@@ -8,15 +7,10 @@ export function Calendar({ selectedDate, onDateChange }) {
   };
 
   return (
-   <p>
-    <span className='today'> Today,</span>
     <DatePicker
       selected={selectedDate}
       onChange={handleDateChange}
       dateFormat="MMMM d"
     />
-   </p>
   );
 }
-
-
